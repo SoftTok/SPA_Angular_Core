287c3e7b90665255a2cd6075ff7c516f0ccf4ad3
ULFBERTO <binarysoft00101@outlook.es>
2025-04-14 23:30:02 -0500

    Refactor project to replace "angular-library-grupo1" with "soft-tok-components", update dependencies in package.json and package-lock.json, and adjust TypeScript configuration and component imports accordingly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AngularLibraryGrupo1Module } from 'node_modules/angular-library-grupo1/src/lib/angular-library-grupo1.module';
+import { AngularLibrarySoftTokModule } from 'node_modules/soft-tok-components/src/stories/components/model-library.module';
 
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
@@ -7,7 +7,7 @@ import { FormularioSignalsComponent } from './components/formulario-signals/form
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormularioSignalsComponent,AngularLibraryGrupo1Module],
+  imports: [RouterOutlet,FormularioSignalsComponent,AngularLibrarySoftTokModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
@@ -41,6 +41,11 @@ export class AppComponent {
       "Calle Falsa 123",
     ]
   ];
+
+
+  generarTabla(){
+    console.log("generarTabla");
+  }
 
 
 }
--- src/types.d.ts
+++
@@ -1,3 +0,0 @@
-// Declaración para la librería angular-library-grupo1
-declare module 'angular-library-grupo1/*';
-declare module 'node_modules/angular-library-grupo1/src/lib/*';
